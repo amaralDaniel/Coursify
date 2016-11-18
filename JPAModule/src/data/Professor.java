9e6dc42e09a85b5f500df90d33143c8cac3c8a4e
Daniel Amaral <amaral94daniel@gmail.com>
2016-11-18 23:31:22 +0000

    Fix relation

--- JPAModule/src/data/Professor.java
+++ JPAModule/src/data/Professor.java
@@ -1,7 +1,9 @@
 package data;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * Created by tomasfrancisco on 13/11/2016.
@@ -13,4 +15,7 @@ public class Professor extends User implements Serializable {
     private String Office;
     private String internalTelephone;
     private Double salary;
+
+    @OneToMany
+    private List<Course> courseList;
 }
