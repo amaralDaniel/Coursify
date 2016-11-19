0f5a2416a5eb4daba5a412fe09e2609c4a02a1ec
Daniel Amaral <amaral94daniel@gmail.com>
2016-11-19 22:11:18 +0000

    Delete Flag Fix Professor Construtor

--- JPAModule/src/data/Flag.java
+++
@@ -1,8 +0,0 @@
-package data;
-
-/**
- * Created by tomasfrancisco on 13/11/2016.
- */
-public enum Flag {
-    ADMIN
-}
--- JPAModule/src/data/Professor.java
+++ JPAModule/src/data/Professor.java
@@ -3,6 +3,7 @@ package data;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.io.Serializable;
+import java.sql.Date;
 import java.util.List;
 
 /**
@@ -11,21 +12,71 @@ import java.util.List;
 @Entity
 public class Professor extends User implements Serializable {
 
-    private String Category;
-    private String Office;
+    private String category;
+    private String office;
     private String internalTelephone;
     private Double salary;
 
     @OneToMany
     private List<Course> courseList;
 
-    public Professor(String name, String email, String passwordHash) {
+    public Professor(String name, Date birthdate, String institutionalEmail, String email, String address,
+                     String telephone, String passwordHash, String category, String office, String internalTelephone,
+                     Double salary ) {
         this.setName(name);
+        this.setInstitutionalEmail(institutionalEmail);
         this.setEmail(email);
+        this.setAddress(address);
+        this.setTelephone(telephone);
         this.setPasswordHash(passwordHash);
+        this.setBirthdate(birthdate);
+        this.setCategory(category);
+        this.setOffice(office);
+        this.setInternalTelephone(internalTelephone);
+        this.setSalary(salary);
     }
 
     public Professor() {
 
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getOffice() {
+        return office;
+    }
+
+    public void setOffice(String office) {
+        this.office = office;
+    }
+
+    public String getInternalTelephone() {
+        return internalTelephone;
+    }
+
+    public void setInternalTelephone(String internalTelephone) {
+        this.internalTelephone = internalTelephone;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
     }
 }
